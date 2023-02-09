0a0f1570cd9f7f4acbca1ca6972609d4867a0415
monil <mdumasia9078@conestogac.on.ca>
2023-02-09 15:09:15 -0500

    initalize commit

---
+++ PROG8170_assignment_2/Circle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG8170_assignment_2
+{
+    public class Circle
+    {
+        private int circle_radius;
+
+        public Circle()
+        {
+            circle_radius = 1;
+        }
+
+        public int GetRadius()
+        {
+            Console.WriteLine("Circle Radius: {0} \n", circle_radius);
+            return circle_radius;
+        }
+
+        public void SetRadius(int radius)
+        {
+            circle_radius = radius;
+            Console.WriteLine("Circle radius set to: {0} \n", circle_radius);
+        }
+
+        public double GetCircumference()
+        {
+            Console.WriteLine("Circle circumference: {0} \n", 2 * circle_radius);
+            return (2 * circle_radius);
+        }
+
+        public double GetArea()
+        {
+            Console.WriteLine("Circle area: {0} \n", 3.14 * circle_radius * circle_radius);
+            return (3.14 * circle_radius * circle_radius);
+        }
+
+    }
+}
---
+++ PROG8170_assignment_2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG8170_assignment_2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                bool isValid = false;
+                int radius = 0;
+                Circle circleObj = new Circle();
+
+                while (!isValid)
+                {
+                    Console.Write("Please enter the circle radius: ");
+
+                    radius = Convert.ToInt32(Console.ReadLine());
+
+                    if (radius < 0 || radius == 0)
+                    {
+                        Console.Write("Please enter valid radius");
+                    }
+                    else
+                    {
+                        isValid = true;
+                        circleObj.SetRadius(radius);
+                    }
+                }
+
+                Console.WriteLine("\t ========================= \t \n");
+
+                Console.WriteLine("1. Get Circle Radius");
+                Console.WriteLine("2. Change Circle Radius");
+                Console.WriteLine("3. Get Circle Circumference");
+                Console.WriteLine("4. Get Circle Area");
+                Console.WriteLine("5. Exit \r\n");
+
+                Console.WriteLine("\t ========================= \t \n");
+
+                bool isLoop = false;
+
+                while (!isLoop)
+                {
+                    Console.Write("Please enter your choice: ");
+                    string choice = Console.ReadLine();
+
+                    switch (choice)
+                    {
+                        case "1":
+                            circleObj.GetRadius();
+                            break;
+                        case "2":
+                            Console.Write("Please enter the circle radius: ");
+
+                            int newRadius = Convert.ToInt32(Console.ReadLine());
+                            if (newRadius < 0 || newRadius == 0)
+                            {
+                                Console.Write("Please enter valid radius");
+                            }
+                            else
+                            {
+                                circleObj.SetRadius(newRadius);
+                            }
+                            break;
+                        case "3":
+                            circleObj.GetCircumference();
+                            break;
+                        case "4":
+                            circleObj.GetArea();
+                            break;
+                        case "5":
+                            System.Environment.Exit(1);
+                            break;
+                        default:
+                            Console.WriteLine("Please enter valid input choice");
+                            break;
+                    }
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
